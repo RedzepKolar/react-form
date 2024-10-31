37f676440b983089b50e1743b999e00fe34f483d
RKolar <bato9440@hotmail.com>
2024-10-31 12:59:07 +0100

    Added states and form functionality

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,14 +1,32 @@
 import CustomInput from "./CustomInput";
-
-/**
- * Napraviti formu za dodavanje gradova i temperatura
- * ime grada
- * drzava
- * temperatura(broj)
- * tehnicki zahtjevi: components, state
- */
+import {useState} from "react";
 
 function Weather() {
+    let [city, setCity] = useState("");
+    let [country, setCountry] = useState("");
+    let [temperature, setTemperature] = useState("");
+    let [cities, setCities] = useState([]);
+
+    function handleAddCity(e){
+        e.preventDefault();
+        if(country === "" || city === "" || temperature === ""){
+            return;
+        }
+
+        setCountry("");
+        setCity("");
+        setTemperature("");
+
+        const newCity = [{
+            name: city,
+            country: country,
+            temperature: temperature,
+        }];
+
+        setCities(currentCities => [...currentCities, ...newCity]);
+
+    }
+
     return(
     <div className="container">
         <div className="row vh-100 d-flex justify-content align-items-center">
@@ -18,13 +36,21 @@ function Weather() {
                         <div className="mb-3 mt-4">
                             <h5 className="fw-bold text-uppercase mb-2">Weather form</h5>
                             <form className="mb-3">
-                                <CustomInput labelName="Ime grada" type="text"/>
-                                <CustomInput labelName="Ime drzave" type="text"/>
-                                <CustomInput labelName="Temperatura" type="number"/>
+                                <CustomInput labelName="City" type="text" value={city}
+                                             onInput={(e) => setCity(e.currentTarget.value)}/>
+                                <CustomInput labelName="Country" type="text" value={country}
+                                             onInput={(e) => setCountry(e.currentTarget.value)}/>
+                                <CustomInput labelName="Temperature" type="number" value={temperature}
+                                             onInput={(e) => setTemperature(e.currentTarget.value)}/>
                                 <div className="d-grid">
-                                    <button className="btn btn-primary" type="submit">
-                                        Unesi
+                                    <button className="btn btn-primary" type="submit" onClick={handleAddCity}>
+                                        Add city temperature
                                     </button>
+                                </div>
+                                <div className="mt-4">
+                                    {cities.map((city, index) => (
+                                        <p key={index}>The temperature in <b>{city.name}</b>, which is located in <b>{city.country}</b>, is currently <b>{city.temperature}°C.</b>  </p>
+                                    ))}
                                 </div>
                             </form>
                         </div>
@@ -32,6 +58,7 @@ function Weather() {
                 </div>
             </div>
         </div>
+
     </div>
     )
 }
